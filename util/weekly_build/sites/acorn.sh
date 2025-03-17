6f4d4e1af1c0b740d4c8934797b5f6cde5aa54ff
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2025-03-17 19:44:03 +0000

    use $INSTALL_OPTS for gh install on cron runner node

--- util/weekly_build/sites/acorn.sh
+++ util/weekly_build/sites/acorn.sh
@@ -6,7 +6,7 @@ function spack_install_wrapper {
   shift 2
   if [[ " $* " =~ " global-workflow-env " ]]; then
     spack config add "config:build_stage:${SPACK_ENV:?}/stage"
-    spack install gh
+    spack install $INSTALL_OPTS gh
   fi
   spack config add 'config:build_stage:$tempdir/$user/spack-stage'
   /opt/pbs/bin/qsub -N spack-build-cache-$RUNID-A -j oe -A NCEPLIBS-DEV -l "select=1:ncpus=12:mem=20GB,walltime=05:00:00" -q dev -V -Wblock=true -- ${SPACK_STACK_DIR}/util/parallel_install.sh 2 6 $*
