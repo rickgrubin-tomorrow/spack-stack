689bc872089e5a362309a5aab13d9def2193c8c4
Steve Grace <sgrace@ucar.edu>
2025-04-01 14:17:42 -0600

    Testing changes

--- util/weekly_build/sites/aws-ubuntu2404.sh
+++ util/weekly_build/sites/aws-ubuntu2404.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-COMPILERS=${COMPILERS:-"intel gcc"}
+COMPILERS=${COMPILERS:-"gcc oneapi"}
 TEMPLATES=${TEMPLATES:-"unified-dev"}
 
 module --force purge
 umask 0022
 
 SPACK_STACK_URL=https://github.com/JCSDA/spack-stack.git
-SPACK_STACK_BRANCH=develop
+SPACK_STACK_BRANCH=feature/hdf5-weekly-build-testing
 
 KEEP_WEEKLY_BUILD_DIR="NO"
 PADDED_LENGTH=200
@@ -17,5 +17,5 @@ function alert_cmd {
   mail -s 'spack-stack weekly build failure' sgrace@ucar.edu  < <(echo "Weekly spack-stack build failed in $1. Run ID: $RUNID")
 }
 
-PACKAGES_TO_TEST="hdf5 zlib-api zlib-ng szip mpi"
+PACKAGES_TO_TEST="hdf5 openmpi"
 PACKAGES_TO_INSTALL="crtm esmf ewok-env global-workflow-env jedi-fv3-env gsi-env"
