9020ef912d2e37323f55d5d6686e9ee592c03a34
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2025-04-02 22:34:03 +0000

    go back to testing pkgs first; add DISABLE_TESTING option

--- util/weekly_build/04_SpackInstall.sh
+++ util/weekly_build/04_SpackInstall.sh
@@ -36,11 +36,11 @@ for compiler in $COMPILERS; do
         --directory ${mirrorpath?"Source mirror path could not be determined. Check site's mirrors.yaml."} \
         ${PACKAGES_TO_INSTALL:---all}
     fi
-    # Install the whole stack, possibly from build cache:
-    spack_install_wrapper log.install install $INSTALL_OPTS $PACKAGES_TO_INSTALL
-    # Force install test packages, do not use build cache:
-    if [[ ! -z "${PACKAGES_TO_TEST}" ]]; then
-      spack_install_wrapper log.install-and-test install $INSTALL_OPTS --no-cache --overwrite --yes-to-all --test root $PACKAGES_TO_TEST
+    # Install test packages; do not use build cache for tested packages:
+    if [[ ( ! -z "${PACKAGES_TO_TEST}" ) && ( "$DISABLE_TESTING" != YES ) ]]; then
+      spack_install_wrapper log.install-and-test install $INSTALL_OPTS --use-buildcache package:never,dependencies:auto --overwrite --yes-to-all --test root $PACKAGES_TO_TEST
     fi
+    # Install the whole stack
+    spack_install_wrapper log.install install $INSTALL_OPTS $PACKAGES_TO_INSTALL
   done
 done
