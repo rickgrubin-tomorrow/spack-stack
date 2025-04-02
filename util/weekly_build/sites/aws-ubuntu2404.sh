badd73b9c376704e2608eee2377bef4b17559564
Steve Grace <sgrace@ucar.edu>
2025-04-02 09:08:50 -0600

    Testing update...

--- util/weekly_build/sites/aws-ubuntu2404.sh
+++ util/weekly_build/sites/aws-ubuntu2404.sh
@@ -3,18 +3,19 @@
 COMPILERS=${COMPILERS:-"gcc oneapi"}
 TEMPLATES=${TEMPLATES:-"unified-dev"}
 
-module --force purge
-umask 0022
+# module --force purge
+# umask 0022
 
-SPACK_STACK_URL=https://github.com/JCSDA/spack-stack.git
+SPACK_STACK_URL=https://github.com/stiggy87/spack-stack.git
 SPACK_STACK_BRANCH=feature/hdf5-weekly-build-testing
 
 KEEP_WEEKLY_BUILD_DIR="NO"
 PADDED_LENGTH=200
 
 function alert_cmd {
-  module purge # annoying libstdc++ issue
-  mail -s 'spack-stack weekly build failure' sgrace@ucar.edu  < <(echo "Weekly spack-stack build failed in $1. Run ID: $RUNID")
+  # module purge # annoying libstdc++ issue
+  # mail -s 'spack-stack weekly build failure' sgrace@ucar.edu  < <(echo "Weekly spack-stack build failed in $1. Run ID: $RUNID")
+  echo "Weekly failure!"
 }
 
 PACKAGES_TO_TEST="hdf5 openmpi"
