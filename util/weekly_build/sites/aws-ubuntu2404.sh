311f373aaefb4fae2bfb0c3e40b9632b07b93c6f
Steve Grace <sgrace@ucar.edu>
2025-04-10 13:59:16 -0600

    removing cleaned up config

--- util/weekly_build/sites/aws-ubuntu2404.sh
+++ util/weekly_build/sites/aws-ubuntu2404.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMPILERS=${COMPILERS:-"gcc oneapi"}
+COMPILERS=${COMPILERS:-"oneapi gcc"}
 TEMPLATES=${TEMPLATES:-"unified-dev"}
 
 # module --force purge
@@ -11,10 +11,10 @@ source /opt/intel/oneapi/setvars.sh --force
 SPACK_STACK_URL=https://github.com/stiggy87/spack-stack.git
 SPACK_STACK_BRANCH=feature/hdf5-weekly-build-testing
 
-KEEP_WEEKLY_BUILD_DIR="NO"
+PADDED_LENGTH=0
+KEEP_WEEKLY_BUILD_DIR="YES"
 
 PACKAGES_TO_TEST="hdf5"
 PACKAGES_TO_INSTALL="ewok-env global-workflow-env jedi-fv3-env"
 FIND_CMD="find"
 TEST_UFSWM=OFF
-KEEP_WEEKLY_BUILD_DIR="YES"
