888aa6200866acc87909f25d11f8249965ed3046
Rick Grubin <rick.grubin@tomorrow.io>
2025-06-27 21:53:28 +0000

    WIP CycleCloud

--- spack-ext/repos/NWP-DA/packages/MiRS/package.py
+++ spack-ext/repos/NWP-DA/packages/MiRS/package.py
@@ -14,9 +14,9 @@ class Mirs(CMakePackage):
 
     maintainers = ["rhoneyager-tomorrow"]
 
-    depends_on('hdf5@1.10.0: +fortran +hl')
+    depends_on('hdf5@1.14.3: +fortran +hl')
     depends_on('netcdf-c')
-    depends_on('zlib-ng')
+    depends_on('zlib')
 
     version('11.10.0.1', commit='e918f984e4d25c954aa3f8f399dadc03815382bb')
     version('11.10.0', commit='ef556263cc782fe2b5b8511789126aba94ab8a31')
